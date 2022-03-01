15484e7708ec0ba247da95c5e7c616663dde9d98
Arthemyst <pawel.lukmin@gmail.com>
2022-03-01 11:26:38 +0100

    Fix: Try to fix .env and settings Error related to secret key

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -1,4 +1,4 @@
-import os.path
+from pathlib import Path
 import environ
 settings_ = """
 Django settings for mysite project.
@@ -21,21 +21,21 @@ env = environ.Env(
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
 
-environ.Env.read_env()
+environ.Env.read_env(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -84,8 +84,7 @@ WSGI_APPLICATION = "conf.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(),
-    'extra': env.db_url('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
+    "default": env.db(default='sqlite:////tmp/my-tmp-sqlite.db')
 }
 
 
