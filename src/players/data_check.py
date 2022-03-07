d560af836467cb16c6a35c4fc3f5e93ae2fdbffc
Arthemyst <pawel.lukmin@gmail.com>
2022-03-07 14:35:49 +0100

    Fix: Fixed prepare_data manegement command. Next step will be preparing feature in command to select common players in all databases from all years.

---
+++ src/players/data_check.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df16 = pd.read_csv(r"2016.csv")
+df16.head()
--- src/players/management/commands/prepare_data.py
+++ src/players/management/commands/prepare_data.py
@@ -19,15 +19,16 @@ class Command(BaseCommand):
             help="Choose directory path when output csv files will save",
         )
 
-    def handle(self, *args, **kwargs):
-        basepath = Path("input")
-        input_files = [f for f in listdir(basepath) if isfile(join(basepath, f))]
-        csv_list = [item for item in listdir(basepath) if item.is_file]
+    def handle(self, *args, **options):
+        basepath = options["input"]
+        input_files = [f for f in listdir(basepath)]
+
+        csv_list = [item for item in listdir(basepath)]
         df_list = list()
 
         for df_csv in csv_list:
             df_field_players = pd.read_csv(
-                f"data_input/{df_csv}",
+                f"{basepath}/{df_csv}",
                 usecols=[
                     "short_name",
                     "nationality",
@@ -73,8 +74,74 @@ class Command(BaseCommand):
                     "value_eur",
                 ],
             )
+            df_field_players = df_field_players[
+                df_field_players["team_position"] != "SUB"
+            ]
+            df_field_players.dropna(subset=["team_position"], inplace=True)
+            df_field_players = df_field_players[
+                df_field_players["team_position"] != "RES"
+            ]
+            df_field_players = df_field_players[
+                df_field_players["player_positions"] != "GK"
+            ]
+            # save team_position as separated data series
+            df_name = df_field_players["short_name"]
+            df_nationality = df_field_players["nationality"]
+            df_club = df_field_players["club"]
+            df_position = df_field_players["team_position"]
+            # drop team_position column for data types optimization
+            df_field_players.drop(
+                [
+                    "player_positions",
+                    "team_position",
+                    "short_name",
+                    "club",
+                    "nationality",
+                ],
+                axis="columns",
+                inplace=True,
+            )
+
+            # optimizing types of data
+            for column in df_field_players.columns:
+                if df_field_players[column].dtypes == "object":
+                    # remove '+' and '-' from columns with parameters values ex. '65+2'
+                    df_field_players[column] = (
+                        df_field_players[column]
+                        .astype("object")
+                        .apply(lambda x: x.split("+")[0])
+                    )
+                    df_field_players[column] = (
+                        df_field_players[column]
+                        .astype("object")
+                        .apply(lambda x: x.split("-")[0])
+                    )
+                    # change data type for integer
+                    df_field_players[column] = df_field_players[column].astype(int)
+                else:
+                    df_field_players[column] = df_field_players[column].astype(int)
+
+                # change player value for millions of euro
+                """
+                df_field_players.loc[:, "value_eur"] *= 0.000001
+                df_field_players.rename(
+                    columns={"value_eur": "value_eur_mln"}, inplace=True
+                )
+                """
+                # add again team_position column to dataframe
+                df_field_players["team_position"] = df_position
+                df_field_players["team_position"] = df_field_players[
+                    "team_position"
+                ].astype("category")
+                df_field_players["club"] = df_club
+                df_field_players["club"] = df_field_players["club"].astype("category")
+                df_field_players["nationality"] = df_nationality
+                df_field_players["nationality"] = df_field_players[
+                    "nationality"
+                ].astype("category")
             df_list.append(df_field_players)
 
         for df, name in zip(df_list, input_files):
 
-            df.to_csv(f"{'output'}20{name[-6::]}", sep=",")
+            df.to_csv(f"{options['output']}20{name[-6::]}", sep=",")
+        print("Data prepared...")
