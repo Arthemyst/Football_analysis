4a8ec9d1d441132e5726a0e10b78cce5cb1f8eb9
Arthemyst <pawel.lukmin@gmail.com>
2022-03-24 16:37:22 +0100

    Feat: Prepared test with pytest module. Tests are checking management command's modules.

--- src/players/management/commands/prepare_data.py
+++ src/players/management/commands/prepare_data.py
@@ -34,103 +34,107 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        basepath = options["input"]
+        csv_files = self.list_items(options["input"])
+        df_list = []
+        for path in csv_files:
+            logging.info(f"Preparing data from {path}...")
+            dataframe = self.read_csv(path)
+
+            dataframe = self.remove_goalkeepers(dataframe)
+
+            dataframe = self.optimize_types(dataframe)
+
+            # add dataframe to list
+            df_list.append(dataframe)
+
+        for dataframe, name in zip(df_list, csv_files):
+
+            self.save_file(dataframe, options["output"], name)
+
+    def list_items(self, directory):
+        # Directory validation and list matching csv files
         try:
-            csv_list = sorted([str(item) for item in list(Path(basepath).iterdir())])
+            return sorted([str(item) for item in list(Path(directory).iterdir())])
         except:
-            raise FileNotFoundError(f"No such file or directory: {options['input']}")
-        df_list = list()
+            raise FileNotFoundError(f"No such file or directory: {directory}")
 
-        for df_csv in csv_list:
-            logging.info(f"Preparing data from {df_csv}...")
-            df_field_players = pd.read_csv(
-                f"{df_csv}",
-                usecols=DEFAULT_COLUMNS,
-                index_col=[0],
+    def read_csv(self, path):
+        # Load a csv into a Pandas dataframe and return it
+        dataframe = pd.read_csv(
+            f"{path}",
+            usecols=DEFAULT_COLUMNS,
+            index_col=[0],
+        )
+        return dataframe
+
+    def remove_goalkeepers(self, dataframe):
+        # remove goalkeepers from dataframe
+        # goalkeepers have different parameters and is not able to compare it with field players
+        dataframe.reset_index(inplace=True)
+        dataframe = dataframe[dataframe["team_position"] != "SUB"]
+        dataframe = dataframe[dataframe["team_position"] != "RES"]
+        dataframe = dataframe[dataframe["player_positions"] != "GK"]
+        dataframe.dropna(subset=["team_position"], inplace=True)
+        return dataframe
+
+    def optimize_types(self, dataframe):
+        # save team_position as separated data series
+        df_name = dataframe["short_name"]
+        df_long_name = dataframe["long_name"]
+        df_nationality = dataframe["nationality"]
+        df_club = dataframe["club"]
+        df_position = dataframe["team_position"]
+        # drop team_position column for data types optimization
+        # dropped columns have object type of data
+        # it make problem with optimization of numerical data in othe columns
+        # dropped columns will be added in other step
+        dataframe.drop(
+            [
+                "player_positions",
+                "team_position",
+                "short_name",
+                "club",
+                "nationality",
+                "long_name",
+            ],
+            axis="columns",
+            inplace=True,
+        )
+
+        # optimizing types of data
+        for column in dataframe.columns:
+            # dataframe.rename({column: f"{column}_{path[8:10]}"})
+            if dataframe[column].dtypes == "object":
+                # remove '+' and '-' from columns with parameters values ex. '65+2'
+                dataframe[column] = (
+                    dataframe[column].astype("object").apply(lambda x: x.split("+")[0])
+                )
+                dataframe[column] = (
+                    dataframe[column].astype("object").apply(lambda x: x.split("-")[0])
+                )
+                # change data type to integer
+                dataframe[column] = dataframe[column].astype(int)
+            else:
+                dataframe[column] = dataframe[column].astype(int)
+
+            # add dropped columns to dataframe
+            dataframe["team_position"] = df_position
+            dataframe["team_position"] = dataframe["team_position"].astype("category")
+            dataframe["club"] = df_club
+            dataframe["club"] = dataframe["club"].astype("category")
+            dataframe["nationality"] = df_nationality
+            dataframe["nationality"] = dataframe["nationality"].astype("category")
+            dataframe["short name"] = df_name
+            dataframe["long_name"] = df_long_name
+        return dataframe
+
+    def save_file(self, dataframe, directory, name):
+        try:
+            dataframe.to_csv(f"{Path(directory)}/20{name[-6::]}", sep=",", index=False)
+            logging.info(
+                f"Prepared new csv file: 20{name[-6::]} for {len(dataframe)} players"
             )
-            df_field_players.reset_index(inplace=True)
-            df_field_players = df_field_players[
-                df_field_players["team_position"] != "SUB"
-            ]
-            df_field_players.dropna(subset=["team_position"], inplace=True)
-            df_field_players = df_field_players[
-                df_field_players["team_position"] != "RES"
-            ]
-            df_field_players = df_field_players[
-                df_field_players["player_positions"] != "GK"
-            ]
-            # save team_position as separated data series
-            df_name = df_field_players["short_name"]
-            df_long_name = df_field_players["long_name"]
-            df_nationality = df_field_players["nationality"]
-            df_club = df_field_players["club"]
-            df_position = df_field_players["team_position"]
-            # drop team_position column for data types optimization
-            df_field_players.drop(
-                [
-                    "player_positions",
-                    "team_position",
-                    "short_name",
-                    "club",
-                    "nationality",
-                    "long_name",
-                ],
-                axis="columns",
-                inplace=True,
+        except:
+            raise OSError(
+                f"Cannot save file into a non-existent directory: {directory}"
             )
-
-            # optimizing types of data
-            for column in df_field_players.columns:
-                df_field_players.rename({column: f"{column}_{df_csv[8:10]}"})
-                if df_field_players[column].dtypes == "object":
-                    # remove '+' and '-' from columns with parameters values ex. '65+2'
-                    df_field_players[column] = (
-                        df_field_players[column]
-                        .astype("object")
-                        .apply(lambda x: x.split("+")[0])
-                    )
-                    df_field_players[column] = (
-                        df_field_players[column]
-                        .astype("object")
-                        .apply(lambda x: x.split("-")[0])
-                    )
-                    # change data type for integer
-                    df_field_players[column] = df_field_players[column].astype(int)
-                else:
-                    df_field_players[column] = df_field_players[column].astype(int)
-
-                # change player value for millions of euro
-                """
-                df_field_players["value_eur"] = df_field_players["value_eur"].apply(
-                    lambda x: x * 0.000001
-                )
-                df_field_players.rename(
-                    columns={"value_eur": "value_eur_mln"}, inplace=True
-                )
-                """
-                # add again team_position column to dataframe
-                df_field_players["team_position"] = df_position
-                df_field_players["team_position"] = df_field_players[
-                    "team_position"
-                ].astype("category")
-                df_field_players["club"] = df_club
-                df_field_players["club"] = df_field_players["club"].astype("category")
-                df_field_players["nationality"] = df_nationality
-                df_field_players["nationality"] = df_field_players[
-                    "nationality"
-                ].astype("category")
-                df_field_players["short name"] = df_name
-                df_field_players["long_name"] = df_long_name
-                # df_field_players.reset_index(inplace=True)
-                # df_field_players.reset_index(inplace=True)
-
-            df_list.append(df_field_players)
-
-        for df, name in zip(df_list, csv_list):
-            try:
-                df.to_csv(f"{options['output']}/20{name[-6::]}", sep=",", index=False)
-            except:
-                raise OSError(
-                    f"Cannot save file into a non-existent directory: {options['output']}"
-                )
-            logging.info(f"Prepared new csv file: 20{name[-6::]} for {len(df)} players")
--- src/players/tests.py
+++ src/players/tests.py
@@ -1,11 +1,58 @@
 import pytest
 import pandas as pd
+from pathlib import Path
+import sys
 
-def test_columns_quantity():
+cwd = Path.cwd()
+# importing sys
 
-    filepath = "./players/data/2015.csv"
-    df = pd.read_csv(filepath)
+
+sys.path.append("./players/management/commands")
+# from prepare_data import Command
+from prepare_data import Command
+
+
+def test_data_optimization():
+    command = Command()
+    filepath = "./players/input_data/players_16.csv"
+    df = command.read_csv(filepath)
+    df = command.remove_goalkeepers(df)
+    df = command.optimize_types(df)
+    assert df["club"].dtypes == "category"
+    assert df["nationality"].dtypes == "category"
+    assert df["age"].dtypes == int
+
+
+def test_read_csv():
+    command = Command()
+    filepath = "./players/input_data/players_16.csv"
+    df = command.read_csv(filepath)
     assert len(df.columns) == 42
 
-def test_example2():
-    assert 1 == 1
+
+def test_remove_goalkeepers():
+    # check removing goalkeepers from dataframe
+    command = Command()
+    filepath = "./players/input_data/players_16.csv"
+    df = pd.read_csv(filepath)
+    df = command.remove_goalkeepers(df)
+    df_gk = df[df["player_positions"] == "GK"]
+    df_sub = df[df["team_position"] == "SUB"]
+    df_res = df[df["team_position"] == "RES"]
+    assert len(df_gk) == 0
+    assert len(df_sub) == 0
+    assert len(df_res) == 0
+
+
+def test_save_file(tmp_path):
+    # check to save prepared dataframe to csv file inside temporary dict
+    command = Command()
+    filepath = "./players/input_data/players_16.csv"
+    df = command.read_csv(filepath)
+    df = command.remove_goalkeepers(df)
+    df = command.optimize_types(df)
+    dir_path = tmp_path / "players_temp"
+    file_path = tmp_path / "players_temp/2016.csv"
+    dir_path.mkdir()
+    command.save_file(df, dir_path, "players_16.csv")
+    assert Path.is_file(file_path) == True
