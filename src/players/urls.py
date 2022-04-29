a39996a3ddd040b9e042788687d020ae91ecd2c8
Arthemyst <pawel.lukmin@gmail.com>
2022-04-29 17:26:39 +0200

    Prepared views: index, player_list and player_detail

---
+++ src/players/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from players.views import PlayerDetailView, PlayerListView
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name='players/index.html'), name="home"),
+    path("players/", PlayerListView.as_view(), name="player-list"),
+    path("players/<int:pk>/", PlayerDetailView.as_view(), name='player-detail')
+]
--- src/players/views.py
+++ src/players/views.py
@@ -1,3 +1,21 @@
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.shortcuts import render
+from players.models import Player, PlayerStatistics
+from typing import Dict, Any
 
-# Create your views here.
+class PlayerListView(ListView):
+
+    model = Player
+    paginate_by = 50
+    context_object_name = 'players'
+
+class PlayerDetailView(DetailView):
+
+    model = Player
+    context_object_name = 'player'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['pace_per_year'] = PlayerStatistics.objects.filter(player=self.get_object()).values_list("year", "pace")
+        return context
