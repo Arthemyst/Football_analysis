9f37418658378a6e2d5ac80c87aa7aa0e46dda88
Arthemyst <pawel.lukmin@gmail.com>
2022-04-30 12:17:08 +0200

    Feat: Add midfielder_list view and add dropdown button.

--- src/players/urls.py
+++ src/players/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from players.views import PlayerDetailView, PlayerListView
+from players.views import PlayerDetailView, PlayerListView, MidfielderListView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name='players/index.html'), name="home"),
     path("players/", PlayerListView.as_view(), name="player-list"),
+    path("midfielders/", MidfielderListView.as_view(), name="midfielder-list"),
     path("players/<int:pk>/", PlayerDetailView.as_view(), name='player-detail')
 ]
