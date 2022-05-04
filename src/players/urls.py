92c25857f190e3d83d9c988f73b6adbb02688abc
Arthemyst <pawel.lukmin@gmail.com>
2022-05-04 21:02:03 +0200

    Feat: Modified index.html template and modified homepage view.

--- src/players/urls.py
+++ src/players/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from players.views import PlayerDetailView, PlayerListView, MidfielderListView, ProfileTemplateView
+from players.views import PlayerDetailView, PlayerListView, MidfielderListView, ProfileTemplateView, HomeView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name='players/index.html'), name="home"),
+    path("", HomeView.as_view(template_name='players/index.html'), name="home"),
     path("midfielders/", MidfielderListView.as_view(), name="midfielder-list"),
     path("players/", PlayerListView.as_view(), name="player-list"),
     path("players/<int:pk>/", PlayerDetailView.as_view(), name='player-detail'),
--- src/players/views.py
+++ src/players/views.py
@@ -11,6 +11,26 @@ class PlayerListView(ListView):
     paginate_by = 50
     context_object_name = 'players'
 
+class HomeView(ListView):
+
+    model = Player
+    context_object_name = 'player'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['players_count'] = Player.objects.all().count()
+        context['2016_count'] = PlayerStatistics.objects.filter(year='2016').count()
+        context['2017_count'] = PlayerStatistics.objects.filter(year='2017').count()
+        context['2018_count'] = PlayerStatistics.objects.filter(year='2018').count()
+        context['2019_count'] = PlayerStatistics.objects.filter(year='2019').count()
+        context['2020_count'] = PlayerStatistics.objects.filter(year='2020').count()
+
+
+
+
+
+
+        return context
 class PlayerDetailView(DetailView):
 
     model = Player
@@ -20,6 +40,7 @@ class PlayerDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['pace_per_year'] = PlayerStatistics.objects.filter(player=self.get_object()).values_list("year", "overall")
         context['team_position'] = PlayerStatistics.objects.filter(player=self.get_object()).values_list("team_position")
+        context['players_count'] = Player.objects.all().count()
 
         return context
 
